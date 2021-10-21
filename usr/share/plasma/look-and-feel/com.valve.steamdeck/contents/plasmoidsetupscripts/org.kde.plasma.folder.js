3355a159b1edc5ae256d4a5b5ed39ffbd1f4a22e
David Edmundson <kde@davidedmundson.co.uk>
2021-10-21 17:42:10 +0100

    Introduce a custom look and feel

---
+++ usr/share/plasma/look-and-feel/com.valve.steamdeck/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
@@ -0,0 +1,5 @@
+applet.wallpaperPlugin = 'org.kde.image'
+applet.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"]
+//applet.writeConfig("Image", "file:///usr/share/wallpapers/your_wallpaper_here.jpg")
+//applet.writeConfig("FillMode", "2")
+applet.reloadConfig()
