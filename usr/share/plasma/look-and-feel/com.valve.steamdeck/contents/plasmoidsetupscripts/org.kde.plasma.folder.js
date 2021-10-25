8458095c18fce73d661ee3435e4b6f2031960afe
David Edmundson <kde@davidedmundson.co.uk>
2021-10-25 16:06:37 +0100

    Set default wallpaper to milkyway
    As this appears to be what an earlier script was trying to do

--- usr/share/plasma/look-and-feel/com.valve.steamdeck/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
+++ usr/share/plasma/look-and-feel/com.valve.steamdeck/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
@@ -1,5 +1,4 @@
 applet.wallpaperPlugin = 'org.kde.image'
 applet.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"]
-//applet.writeConfig("Image", "file:///usr/share/wallpapers/your_wallpaper_here.jpg")
-//applet.writeConfig("FillMode", "2")
+applet.writeConfig("Image", "/usr/share/wallpapers/MilkyWay/")
 applet.reloadConfig()
