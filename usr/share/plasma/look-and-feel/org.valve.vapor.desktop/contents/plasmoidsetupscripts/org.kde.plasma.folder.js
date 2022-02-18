e56a6e6b40769178ea418007b4e2f1606a5059cb
David Redondo <kde@david-redondo.de>
2022-02-18 09:54:49 +0100

    Import Vapor theme assets and make it the default

--- usr/share/plasma/look-and-feel/org.valve.vapor.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
+++ usr/share/plasma/look-and-feel/org.valve.vapor.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
@@ -1,4 +1,4 @@
 applet.wallpaperPlugin = 'org.kde.image'
 applet.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"]
-applet.writeConfig("Image", "/usr/share/wallpapers/MilkyWay/")
+applet.writeConfig("Image", "/usr/share/wallpapers/MilkyWay/") // FIXME once we get the actual default wallpaper
 applet.reloadConfig()
