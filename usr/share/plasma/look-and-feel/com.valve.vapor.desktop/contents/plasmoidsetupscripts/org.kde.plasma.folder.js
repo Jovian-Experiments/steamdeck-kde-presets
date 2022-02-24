32b2c52017543ffbc658cc0ad42d5eef392ca2ca
David Redondo <kde@david-redondo.de>
2022-02-24 08:51:55 +0100

    vapor: Use the default wallpaper

--- usr/share/plasma/look-and-feel/com.valve.vapor.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
+++ usr/share/plasma/look-and-feel/com.valve.vapor.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
@@ -1,4 +1,4 @@
 applet.wallpaperPlugin = 'org.kde.image'
 applet.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"]
-applet.writeConfig("Image", "/usr/share/wallpapers/MilkyWay/") // FIXME once we get the actual default wallpaper
+applet.writeConfig("Image", "/usr/share/wallpapers/Steam Deck Logo Default.jpg")
 applet.reloadConfig()
